b07502ff3134086155bd8c1890a7deeaccbed43d
DomantasB <kdomanta@amazon.com>
2024-01-26 08:57:16 +0200

    add build option to dev deploy. change permissions locally should work for more yarn configurations

--- src/commands/devDeploy.ts
+++ src/commands/devDeploy.ts
@@ -1,26 +1,39 @@
-import { ArgsOf, bindCommand, Command } from '../utils/command';
-import { Options } from 'yargs';
-import { withStage } from '../utils/stage';
-import { commandExec } from '../utils/exec';
-import { adaAuth } from '../actions/adaAuth';
+import { ArgsOf, bindCommand, Command } from "../utils/command";
+import { option, withStage } from "../utils/stage";
+import { commandExec } from "../utils/exec";
+import { adaAuth } from "../actions/adaAuth";
 
-@bindCommand('dev-deploy [stack] deploys specified stack to AWS')
+@bindCommand("dev-deploy [stack] deploys specified stack to AWS")
 export class DevDeploy extends Command {
   builder() {
     return withStage({
-      inclusive: {
-        type: 'boolean',
-        alias: 'i',
+      inclusive: option({
+        boolean: true,
+        alias: "i",
         default: false,
-        describe: 'whether to include stack dependencies into the deployment process'
-      } as Options
+        describe:
+          "whether to include stack dependencies into the deployment process",
+      }),
+      build: option({
+        boolean: true,
+        alias: "b",
+        default: false,
+        describe: "whether to build the application before deploying or not",
+      }),
     });
   }
 
   async handler(args: ArgsOf<this>) {
-    const stack = this.argAt(1, 'Usage: jst dev-deploy [stack]');
+    const stack = this.argAt(1, "Usage: jst dev-deploy [stack]");
 
     await adaAuth();
-    await commandExec(`cdk deploy "${stack}" --require-approval never ${args.inclusive ? '' : '--exclusively'}`)
+    if (args.build) {
+      await commandExec(`brazil-build`);
+    }
+    await commandExec(
+      `cdk deploy "${stack}" --require-approval never ${
+        args.inclusive ? "" : "--exclusively"
+      }`,
+    );
   }
 }
