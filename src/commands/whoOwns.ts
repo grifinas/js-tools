eeaec855254d45a9d59ed92b3ad8f65956e6681b
DomantasB <kdomanta@amazon.com>
2025-08-22 13:18:28 +0300

    add region to who-owns

--- src/commands/whoOwns.ts
+++ src/commands/whoOwns.ts
@@ -13,21 +13,30 @@ export class WhoOwns extends Command {
         alias: "a",
         description: "If passed, will find all instances of resource",
       }),
+      region: option({
+        string: true,
+        default: "us-east-1",
+        alias: "r",
+        description: "Region to check",
+      }),
     });
   }
 
   async handler(args: ArgsOf<this>): Promise<void> {
     const resource = this.argAt<string>(1, "Usage jst who-owns [resource]");
     await adaAuth();
-    const res = await commandExec("aws cloudformation describe-stacks", {
-      quiet: true,
-    });
+    const res = await commandExec(
+      `aws cloudformation describe-stacks --region=${args.region}`,
+      {
+        quiet: true,
+      },
+    );
     const stacks = JSON.parse(res.join(""));
 
     for (const stack of stacks.Stacks) {
       const stackTemplate = (
         await commandExec(
-          `aws cloudformation get-template --stack-name=${stack.StackName}`,
+          `aws cloudformation get-template --stack-name=${stack.StackName} --region=${args.region}`,
           { quiet: true },
         )
       ).join("");
